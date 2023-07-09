0c2a79ee18e57234d226eb79c5994895400733ce
Marco Matthies <71844+marcom@users.noreply.github.com>
2023-07-09 10:31:49 +0200

    Add unittests for vienna_to_pairs

--- unittest/test-vienna_to_pairs.c
+++ unittest/test-vienna_to_pairs.c
@@ -6,6 +6,44 @@
 
 #define UP NA_UNPAIRED
 
+void test_vienna_to_pairs(void) {
+    char *vienna = "(((...)))";
+    uint pairs[] = {-1, -1, -1, -1, -1, -1, -1, -1, -1};
+    uint expected_pairs[] = {8, 7, 6, UP, UP, UP, 2, 1, 0};
+    uint n = strlen(vienna);
+    bool verbose = true;
+    int ret = vienna_to_pairs(n, vienna, verbose, pairs);
+    TEST_ASSERT_EQUAL_UINT_ARRAY(expected_pairs, pairs, n);
+    TEST_ASSERT_EQUAL_INT(EXIT_SUCCESS, ret);
+}
+
+void test_vienna_to_pairs_fails_missing_closing_parens(void) {
+    char *vienna = "(((...)).";
+    uint pairs[] = {-1, -1, -1, -1, -1, -1, -1, -1, -1};
+    uint n = strlen(vienna);
+    bool verbose = true;
+    int ret = vienna_to_pairs(n, vienna, verbose, pairs);
+    TEST_ASSERT_EQUAL_INT(EXIT_FAILURE, ret);
+}
+
+void test_vienna_to_pairs_fails_too_many_closing_parens(void) {
+    char *vienna = "(.(...)))";
+    uint pairs[] = {-1, -1, -1, -1, -1, -1, -1, -1, -1};
+    uint n = strlen(vienna);
+    bool verbose = true;
+    int ret = vienna_to_pairs(n, vienna, verbose, pairs);
+    TEST_ASSERT_EQUAL_INT(EXIT_FAILURE, ret);
+}
+
+void test_vienna_to_pairs_fails_illegal_character(void) {
+    char *vienna = "(([...]))";
+    uint pairs[] = {-1, -1, -1, -1, -1, -1, -1, -1, -1};
+    uint n = strlen(vienna);
+    bool verbose = true;
+    int ret = vienna_to_pairs(n, vienna, verbose, pairs);
+    TEST_ASSERT_EQUAL_INT(EXIT_FAILURE, ret);
+}
+
 void test_xvienna_to_pairs(void) {
     char *vienna = "(((...)))";
     uint pairs[] = {-1, -1, -1, -1, -1, -1, -1, -1, -1};
@@ -18,6 +56,10 @@ void test_xvienna_to_pairs(void) {
 int main(void)
 {
     UNITY_BEGIN();
+    RUN_TEST(test_vienna_to_pairs);
+    RUN_TEST(test_vienna_to_pairs_fails_missing_closing_parens);
+    RUN_TEST(test_vienna_to_pairs_fails_too_many_closing_parens);
+    RUN_TEST(test_vienna_to_pairs_fails_illegal_character);
     RUN_TEST(test_xvienna_to_pairs);
     return UNITY_END();
 }
