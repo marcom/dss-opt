fa4d3121ea17e15da43c41bff45fa000cc8279ac
Marco Matthies <71844+marcom@users.noreply.github.com>
2023-07-16 16:02:49 +0200

    Python opt_md: better checking of seq_constraints_hard

--- python/dssopt/lib.py
+++ python/dssopt/lib.py
@@ -2,6 +2,7 @@ from typing import Union
 import pathlib
 import ctypes
 from ctypes import CDLL, byref, c_bool, c_char_p, c_int, c_uint, c_double, create_string_buffer, pointer
+c_uint_p = ctypes.POINTER(c_uint)
 
 libdssopt_path = str(pathlib.Path(__file__).parent / "libdssopt.so")
 #print(f"__file__ = {__file__}")
@@ -18,18 +19,78 @@ DEFAULT_DSSOPT_kpur = c_double.in_dll(libdssopt, 'DEFAULT_DSSOPT_kpur').value
 DEFAULT_DSSOPT_khet = c_double.in_dll(libdssopt, 'DEFAULT_DSSOPT_khet').value
 DEFAULT_DSSOPT_het_window = c_uint.in_dll(libdssopt, 'DEFAULT_DSSOPT_het_window').value
 
+# C function argtypes and restype
+# parse_seq_constraints_hard
+libdssopt.parse_seq_constraints_hard.argtypes = [
+    c_uint,    # n
+    c_uint_p,  # *hard
+    c_uint_p,  # *n_hard
+    c_char_p,  # *constraint_str
+    c_bool,    # verbose
+    c_uint_p,  # *pairs
+]
+libdssopt.parse_seq_constraints_hard.restype = c_uint
+# vienna_to_pairs
+libdssopt.vienna_to_pairs.argtypes = [
+    c_uint,    # n
+    c_char_p,  # *vienna
+    c_bool,    # verbose
+    c_uint_p,  # *pairs
+]
+libdssopt.vienna_to_pairs.restype = c_int
+# run_md
+libdssopt.run_md.argtypes = [
+    c_char_p,  # *vienna
+    c_char_p,  # *seq_constraints_hard
+    c_uint,    # nsteps
+    c_uint,    # nprint
+    c_uint,    # ncool
+    c_uint,    # npur
+    c_double,  # timestep
+    c_double,  # T_start
+    c_double,  # kpi
+    c_double,  # kpa
+    c_double,  # kneg
+    c_double,  # khet
+    c_uint,    # het_window
+    c_double,  # kpur_end
+    c_bool,    # do_exp_cool
+    c_bool,    # do_movie_output
+    c_bool,    # verbose
+    c_char_p,  # *designed_seq
+]
+libdssopt.run_md.restype = c_uint
+
+
 def list_to_carray(lst, ctype=c_int):
     return (ctype * len(lst))(*lst)
 
+def isok_seq_constraints(constraints: str, vienna: str) -> bool:
+    if constraints is None:
+        return True
+    if len(constraints) != len(vienna):
+        return False
+    n = len(constraints)
+    c_n = c_uint(n)
+    hard = list_to_carray([0] * n, c_uint)
+    n_hard = c_uint(0)
+    pairs = list_to_carray([0] * n, c_uint)
+    verbose = c_bool(True)
+    c_constraints = constraints.encode('utf-8')
+    status = libdssopt.parse_seq_constraints_hard(c_n, hard, n_hard, c_constraints, verbose, pairs)
+    if status == C_EXIT_SUCCESS:
+        return True
+    return False
+
 def opt_md(target_dbn: str,
            seq_constraints_hard: Union[str, None] = None,
-           # taken from main-opt-md.c
+           # default values taken from main-opt-md.c
            time_total: float = 50.0,
            time_print: float = 2.5,
            time_cool: Union[float, None] = None,
            time_pur: Union[float, None] = None,
-           timestep: float   = 0.0015,
-           T_start: float    = 40.0,
+           timestep: float = 0.0015,
+           T_start: float = 40.0,
            kpi: float = DEFAULT_DSSOPT_kpi,
            kpa: float = DEFAULT_DSSOPT_kpa,
            kneg: float = DEFAULT_DSSOPT_kneg,
@@ -41,19 +102,18 @@ def opt_md(target_dbn: str,
            seed: Union[int, None] = None,
            verbose: bool = False,
            ) -> str:
-    # TODO
-    # - checks
-    #   - seq_constraints_hard
     if time_cool == None:
         time_cool = 0.1 * time_total
     if time_pur == None:
         time_pur = 0.8 * time_total
     if time_total < 0 or time_print < 0 or time_cool < 0:
-        raise Exception("All times must be >= 0")
+        raise Exception('All times must be >= 0')
     if timestep < 0:
-        raise Exception("timestep must be >= 0")
+        raise Exception('timestep must be >= 0')
     if het_window < 0:
-        raise Exception("het_window must be >= 0")
+        raise Exception('het_window must be >= 0')
+    if not isok_seq_constraints(seq_constraints_hard, target_dbn):
+        raise Exception(f'seq_constraints_hard are illegal\ntarget_dbn  = {target_dbn}\nconstraints = {seq_constraints_hard}')
     nsteps = c_uint(round(time_total / timestep))
     nprint = c_uint(round(time_print / timestep))
     ncool  = c_uint(round(time_cool  / timestep))
@@ -66,16 +126,12 @@ def opt_md(target_dbn: str,
     T_start = c_double(T_start)
     vienna = target_dbn.encode('utf-8')
     if seq_constraints_hard == None:
-        seq_constraints_hard = ctypes.POINTER(ctypes.c_char)()
+        seq_constraints_hard = c_char_p()
     else:
         seq_constraints_hard = seq_constraints_hard.encode('utf-8')
 
     # + 1 for C-style 0-terminated strings
     designed_seq_buf = create_string_buffer(len(target_dbn) + 1)
-    libdssopt.run_md.argtypes = [c_char_p, c_char_p, c_uint, c_uint, c_uint, c_uint,
-                                 c_double, c_double, c_double, c_double, c_double, c_double, c_uint, c_double,
-                                 c_bool, c_bool, c_bool, c_char_p]
-    libdssopt.run_md.restype = c_uint
     status = libdssopt.run_md(vienna, seq_constraints_hard, nsteps, nprint, ncool, npur,
                               timestep, T_start, kpi, kpa, kneg, khet, het_window, kpur_end,
                               do_exp_cool, do_movie_output, verbose, designed_seq_buf)
@@ -84,7 +140,7 @@ def opt_md(target_dbn: str,
     designed_seq = designed_seq_buf.value.decode('utf-8')
     return designed_seq
 
-def random_vienna(n: int, hpmin: int=3):
+def random_vienna(n: int, hpmin: int=3) -> str:
     pairs = list_to_carray([0] * n, c_uint)
     libdssopt.random_pairs(n, byref(pairs), hpmin)
     # + 1 for C-style 0-terminated strings
