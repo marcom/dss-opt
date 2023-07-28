9e135805e68d1f650114e43de7ecf5ff7ad9ee1c
Marco Matthies <71844+marcom@users.noreply.github.com>
2023-07-28 12:19:41 +0200

    Add -q option to eval-useq for quiet output

--- main-eval-useq.c
+++ main-eval-useq.c
@@ -7,15 +7,21 @@ int main(int argc, char **argv)
     uint n, seq_len, *useq;
     char *vienna, *seq;
     int G;
+    bool quiet = false;
     struct nn_inter *inter;
 
-    if (argc != 3) {
+    if (argc == 3) {
+        vienna = argv[1];
+        seq = argv[2];
+    } else if (argc == 4 && (strcmp(argv[1], "-q") == 0)) {
+        quiet = true;
+        vienna = argv[2];
+        seq = argv[3];
+    } else {
         printf("ERROR: wrong number of arguments\n"
-               "usage: %s vienna seq\n", argv[0]);
+               "usage: %s [-q] vienna seq\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-    vienna = argv[1];
-    seq = argv[2];
     n = strlen(vienna);
     seq_len = strlen(seq);
     if (n != seq_len) {
@@ -29,15 +35,20 @@ int main(int argc, char **argv)
     xvienna_to_pairs(n, vienna, inter->pairs);
     find_interactions(inter);
 
-    printf("vienna = %s\n", vienna);
-    printf("seq    = %s\n", seq);
-    printf("\n");
-    print_interactions(inter);
-    printf("\n");
+    if (! quiet) {
+        printf("vienna = %s\n", vienna);
+        printf("seq    = %s\n", seq);
+        printf("\n");
+        print_interactions(inter);
+        printf("\n");
+    }
 
     xstr_to_useq(n, seq, useq);
     G = calc_interactions_useq(inter, useq);
-    printf("G = %5.2f\n", G * 0.01);
+    if (! quiet) {
+        printf("G = ");
+    }
+    printf("%5.2f\n", G * 0.01);
 
     free(useq);
     nn_inter_delete(inter);
