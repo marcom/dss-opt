7d277319e3da5b8566e4c41a8a5c3b2981ccc11b
Marco Matthies <71844+marcom@users.noreply.github.com>
2023-07-29 18:20:32 +0200

    Python unittest for calc_interactions_useq

---
+++ python/tests/test_calc_interactions_useq.py
@@ -0,0 +1,43 @@
+import pytest
+import dssopt
+
+@pytest.mark.parametrize(
+    "kwargs, expected",
+    [
+        # same tests as in unittest/test-calc_interactions_useq.c
+        # hairpins
+        ({'seq': 'GAAAC', 'dbn': '(...)'}, 570),
+        ({'seq': 'GAAAAC', 'dbn': '(....)'}, 450),
+        ({'seq': 'GAAAAAAC', 'dbn': '(......)'}, 430),
+        # special hairpins
+        ({'seq': 'GGAAAC', 'dbn': '(....)'}, 20),
+        # stacks
+        ({'seq': 'GGGAAACCC', 'dbn': '(((...)))'}, -90),
+        ({'seq': 'CGGAAACCG', 'dbn': '(((...)))'},   0),
+        ({'seq': 'GGGGUUUUCCCC', 'dbn': '((((....))))'}, -580),
+        ({'seq': 'GGCGUUAUCGCC', 'dbn': '((((....))))'}, -500),
+        ({'seq': 'GACGUUAUCGUC', 'dbn': '((((....))))'}, -290),
+        # bulges
+        ({'seq': 'GGGGAAAAAUCCC', 'dbn': '(((.(....))))'},   10),
+        ({'seq': 'GGGGAAAACACCC', 'dbn': '((((....).)))'}, -160),
+        # internal loops
+        ({'seq': 'GGAGGAAAACACCC', 'dbn': '((.((....).)))'},  220),
+        ({'seq': 'GGAGGAAAACCACC', 'dbn': '((.((....)).))'}, -170),
+        # multiway loops
+        ({'seq': 'GGAAGGGAAAACCCGGGAAACCCACC', 'dbn': '((..(((....)))(((...))).))'},   -720),        
+        ({'seq': 'GGAAACGAAAACGUGGGAAACCCAUC', 'dbn': '((..(((....)))(((...))).))'},    -70),        
+        ({'seq': 'GGAAACGAAAACGUAGGGAAACCCAUC', 'dbn': '((..(((....))).(((...))).))'},  -90),
+        ({'seq': 'GGAGGAAAACCAACGAAAACGUAGUGAAACGCAUC', 'dbn': '((.((....)).(((....))).(((...))).))'}, 150),
+        # empty structures
+        ({'seq': 'A',      'dbn': '.'},      0),
+        ({'seq': 'CA',     'dbn': '..'},     0),
+        ({'seq': 'CAA',    'dbn': '...'},    0),
+        ({'seq': 'UGAC',   'dbn': '....'},   0),
+        ({'seq': 'GGACC',  'dbn': '.....'},  0),
+        ({'seq': 'AGGACC', 'dbn': '......'}, 0),
+    ]
+)
+def test_calc_interactions_useq(kwargs, expected):
+    print(kwargs)
+    result = dssopt.calc_interactions_useq(**kwargs)
+    assert result == expected
