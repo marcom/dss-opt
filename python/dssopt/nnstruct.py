f17d4393d64661ace86117b8a1201da413d2bc27
Marco Matthies <71844+marcom@users.noreply.github.com>
2023-07-31 16:28:55 +0200

    Python: add NNstruct methods dGdp, dGdp_from_str, and seqargmax

--- python/dssopt/nnstruct.py
+++ python/dssopt/nnstruct.py
@@ -13,12 +13,21 @@ def one_hot_seq(s: str, alphabet: str='ACGU', dtype=np.dtype('float64')) -> np.n
         p[i,j] = 1.0
     return p
 
+def seqargmax(pseq: np.ndarray, alphabet: str='ACGU'):
+    if pseq.ndim != 2:
+        raise RuntimeError('input array shape must have ndim = 2')
+    (n, na) = pseq.shape
+    if len(alphabet) != na:
+        raise RuntimeError(f'array and alphabet have incompatible lengths, {na} != {len(alphabet)}')
+    return ''.join(list(map(lambda i: alphabet[i], np.argmax(pseq, axis=1))))
+
 class NNstruct(lib.struct_nn_inter):
     def __init__(self, dbn: str):
         self.n = len(dbn)
         self.nbase = lib.NA_NBASES.value
         self.inter = lib.nn_inter_xnew(self.n)
         self.dbn = dbn
+        self._wrap_out_dGdp = None
         ret = lib.vienna_to_pairs(self.n, self.dbn, False, self.inter.contents.pairs)
         if ret != lib.C_EXIT_SUCCESS:
             raise RuntimeError("vienna_to_pairs failed: n={self.n} vienna={self.dbn}")
@@ -47,3 +56,23 @@ class NNstruct(lib.struct_nn_inter):
             raise RuntimeError(f'seq must have length {self.n}, but has length {len(seq)}')
         pseq = one_hot_seq(seq)
         return self.energy_pseq(pseq)
+
+    def dGdp(self, pseq: np.ndarray) -> np.ndarray:
+        if pseq.shape != (self.n, self.nbase):
+            raise RuntimeError(f'pseq must have shape ({self.n}, {self.nbase})')
+        if pseq.dtype != np.dtype('float64'):
+            raise RuntimeError('pseq must have dytpe float64')
+        if self._wrap_out_dGdp is None:
+            self._wrap_out_dGdp = lib.c_npmat(np.zeros_like(pseq))
+        else:
+            self._wrap_out_dGdp[:] = 0.0
+        wrap_pseq = lib.c_npmat(pseq)
+        # wrap_out_dGdp = lib.c_npmat(self._out_dGdp)
+        lib.calc_interactions_dGdp_pseq(self.inter, wrap_pseq.ctype_arr, self._wrap_out_dGdp.ctype_arr)
+        return self._wrap_out_dGdp.arr
+
+    def dGdp_from_str(self, seq: str) -> np.ndarray:
+        if len(seq) != self.n:
+            raise RuntimeError(f'seq must have length {self.n}, but has length {len(seq)}')
+        pseq = one_hot_seq(seq)
+        return self.dGdp(pseq)
